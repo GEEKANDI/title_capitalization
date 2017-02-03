3464dc0c0e9b054904c3487b5f30d5c7a3c0bb80
Geekandi <geekash254@gmail.com>
2017-02-03 15:24:03 +0300

    Debugged error of first word being in the special character and added a user input prompt

--- title_capitalization.py
+++ title_capitalization.py
@@ -1,8 +1,6 @@
-# import sys
-#
-# title = input('Input the title to capitalize')
+import sys
 
-title = 'wHY DoeS A biRd Fly To The bush'
+title = input('Please type the title to capitalize: \n')
 
 # special word which should not be capitalized while within a sentence
 special_list = ['a', 'the', 'to', 'at', 'in', 'with', 'and', 'but', 'or']
@@ -10,18 +8,21 @@ special_list = ['a', 'the', 'to', 'at', 'in', 'with', 'and', 'but', 'or']
 # Split the title string by spaces
 words = title.split(' ')
 
-# Capitalize the first and last word
-words[0] = words[0].capitalize()
-words[-1]= words[-1].capitalize()
-
 # len(word)-1 in the for loop ensure that the first and last word are not affected
 for i in range(len(words)-1):
 
+    # Capitalize the first and last word
+    words[0] = words[0].capitalize()
+    words[-1] = words[-1].capitalize()
+
+    # Lowercase words in the special list
     if words[i].lower() in special_list:
         words[i] = words[i].lower()
+    # Capitalize words that aren't in the special list
     else:
         words[i] = words[i].capitalize()
     i +=1
 
+# finally joins all the words with a space and print it to the user
 print(' '.join(words))
 
